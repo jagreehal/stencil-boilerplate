8818e9d3a1e11aefbeab679dfa3542ca05617bb9
Jag Reehal <jag.reehal@gmail.com>
2018-04-05 13:37:43 +0100

    @stencil/core v0.7.12 😋

--- src/components/my-app/my-app.spec.tsx
+++ src/components/my-app/my-app.spec.tsx
@@ -16,11 +16,25 @@ describe('my-app', () => {
     });
 
     it('should work with both the first and the last name', async () => {
-      element.first = 'Peter'
+      element.first = 'Peter';
       element.last = 'Parker';
       await flush(element);
       expect(element.textContent).toEqual('Hello, my name is Peter Parker');
     });
   });
-});
 
+  describe('passing in props', () => {
+    let element;
+    beforeEach(async () => {
+      element = await render({
+        components: [MyApp],
+        html: '<my-app first="Peter" last="Parker"><my-app>'
+      });
+    });
+
+    it('should be able to pass in prop values in tests', async () => {
+      await flush(element);
+      expect(element.textContent).toEqual('Hello, my name is Peter Parker');
+    });
+  });
+});
