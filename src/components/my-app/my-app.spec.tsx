c16e4ad86d040e6ff453a400e29caf81c004cd04
Jag Reehal <jag.reehal@gmail.com>
2018-05-03 12:48:10 +0100

    stencil-8.0

--- src/components/my-app/my-app.spec.tsx
+++ src/components/my-app/my-app.spec.tsx
@@ -1,4 +1,4 @@
-import { render, flush } from '@stencil/core/testing';
+import { TestWindow } from '@stencil/core/testing';
 import { MyApp } from './my-app';
 
 describe('my-app', () => {
@@ -8,32 +8,20 @@ describe('my-app', () => {
 
   describe('rendering', () => {
     let element;
+    let window;
+
     beforeEach(async () => {
-      element = await render({
+      window = new TestWindow();
+      element = await window.load({
         components: [MyApp],
-        html: '<my-app><my-app>'
+        html: '<my-app></my-app>'
       });
     });
 
     it('should work with both the first and the last name', async () => {
       element.first = 'Peter';
       element.last = 'Parker';
-      await flush(element);
-      expect(element.textContent).toEqual('Hello, my name is Peter Parker');
-    });
-  });
-
-  describe('passing in props', () => {
-    let element;
-    beforeEach(async () => {
-      element = await render({
-        components: [MyApp],
-        html: '<my-app first="Peter" last="Parker"><my-app>'
-      });
-    });
-
-    it('should be able to pass in prop values in tests', async () => {
-      await flush(element);
+      await window.flush();
       expect(element.textContent).toEqual('Hello, my name is Peter Parker');
     });
   });
