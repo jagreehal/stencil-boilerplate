f1d792048cc77a9c0eb1b96e9fb6b69f6e0129f1
jagreehal <jag.reehal@gmail.com>
2018-10-14 23:29:25 +0100

    v0.14.0-5

--- src/components/my-app/my-app.stories.tsx
+++ src/components/my-app/my-app.stories.tsx
@@ -1,20 +1,12 @@
-import {
-    storiesOf
-} from '@storybook/html';
+import { storiesOf } from '@storybook/html';
 
-import {
-    withKnobs,
-    text,
-} from '@storybook/addon-knobs';
-
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 storiesOf('My App', module)
-    .addDecorator(withKnobs)
-    .add('Example', () => {
-        const first = text('First', 'Jag')
-        const last = text('Last', 'Reehal')
+  .addDecorator(withKnobs)
+  .add('Example', () => {
+    const first = text('First', 'Jag');
+    const last = text('Last', 'Reehal');
 
-        return (
-            `<my-app first="${first}" last="${last}" ></my-app>`
-        )
-    })
+    return `<my-app first="${first}" last="${last}" ></my-app>`;
+  });
